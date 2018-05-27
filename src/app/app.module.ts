98c1ed7841a8c435062f813647cfd88ee1f69aba
Dima Mosby <dimakovalevskyi@gmail.com>
2018-05-27 14:47:53 +0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule
+} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
